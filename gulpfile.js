02938e0d2d11f2ca1a277b0af1e6c7ffb801bf34
masha-feilyk <mf@rademade.com>
2018-02-05 01:12:57 +0200

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
-var gulp        = require('gulp'); // Подключаем Gulp
-    sass        = require('gulp-sass'); //Подключаем Sass пакет,
-    pug         = require('gulp-pug'); // Подключаем pug пакет
-    inject      = require('gulp-inject');
-    browserSync = require('browser-sync'); // Подключаем Browser Sync
-    concat      = require('gulp-concat'); // Подключаем gulp-concat (для конкатенации файлов)
-    uglify      = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
-    watch       = require('gulp-watch');
-    imagemin    = require('gulp-imagemin');
-    svgSprite   = require('gulp-svg-sprite');
-    rimraf      = require('rimraf');
-    reload      = browserSync.reload;
+var gulp        = require('gulp'); // Подключаем 'Gulp'
+    sass        = require('gulp-sass'); //Подключаем 'Sass'
+    pug         = require('gulp-pug'); // Подключаем 'Pug'
+    browserSync = require('browser-sync'); // Подключаем 'Browser Sync'
+    watch       = require('gulp-watch'); // Подключаем 'Watch', чтобы следить за изменением файлов
+    imagemin    = require('gulp-imagemin'); // Подключаем 'Imagemin' для минификации изображений
+    svgSprite   = require('gulp-svg-sprite'); // Подключаем 'Svg-sprite' для svg спрайтов
+    rimraf      = require('rimraf'); // Подключаем 'Rimraf' для удаление папок
 
+
+// Виносим все пути к нужным папкам в переменную
 var path = {
     build: {
         html: 'build/',
@@ -36,6 +34,7 @@ var path = {
     clean: './build'
 };
 
+// Конфигурация для локального сервера
 var config = {
     server: {
         baseDir: "./build"
@@ -45,59 +44,70 @@ var config = {
     port: 3000
 };
 
+// Таск для запуска сервера
 gulp.task('webserver', function () {
     browserSync(config);
 });
 
+// Таск для удаления папки 'build'
 gulp.task('clean', function (cb) {
     rimraf(path.clean, cb);
 });
 
+// Таск для сборки html
 gulp.task('html:build', function () {
-    gulp.src(path.src.html)
-        .pipe(pug())
-        .pipe(gulp.dest(path.build.html))
+    gulp.src(path.src.html) // Берем все файли .pug
+        .pipe(pug()) // Конвертируем их в .html
+        .pipe(gulp.dest(path.build.html)) // Ложим их в папку 'build'
         .pipe(browserSync.reload({
-            stream: true
+            stream: true // Перезапускаем сервер
         }))
 });
 
+// Таск для сборки css
 gulp.task('sass:build', function () {
-    gulp.src(path.src.sass)
-        .pipe(sass())
-        .pipe(gulp.dest(path.build.css))
+    gulp.src(path.src.sass) // Берем все файли .sass
+        .pipe(sass()) // Конвертируем их в .css
+        .pipe(gulp.dest(path.build.css)) // Ложим их в папку 'build'
         .pipe(browserSync.reload({
-            stream: true
+            stream: true // Перезапускаем сервер
         }))
 });
 
+// Таск для минификации картинок
 gulp.task('image:build', function () {
-    gulp.src(path.src.img)
-        .pipe(imagemin())
-        .pipe(gulp.dest(path.build.img))
+    gulp.src(path.src.img) // Берем все картинки
+        .pipe(imagemin()) // Минифицируем их
+        .pipe(gulp.dest(path.build.img)) // Ложим их в папку 'build'
         .pipe(browserSync.reload({
-            stream: true
+            stream: true // Перезапускаем сервер
         }))
 });
 
+// Таск для сборки спрайтов
 gulp.task('sprite:build', function () {
-    gulp.src(path.src.sprite)
+    gulp.src(path.src.sprite) // Берем все иконки .svg
         .pipe(svgSprite({
             mode: {
-                symbol: true
+                symbol: true // Конвертируем их в <symbol> для инлайнового спрайта
             }
         }))
-        .pipe(gulp.dest('src/template/sprites/'))
+        .pipe(gulp.dest('src/template/sprites/')) // Ложим их в папку 'sprites'
         .pipe(browserSync.reload({
-            stream: true
+            stream: true // Перезапускаем сервер
         }))
 });
 
+// Таск для сборки шрифтов
 gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+    gulp.src(path.src.fonts) // Берем шрифты
+        .pipe(gulp.dest(path.build.fonts)) // Ложим их в папку 'build'
+        .pipe(browserSync.reload({
+            stream: true // Перезапускаем сервер
+        }))
 });
 
+// В этом таске собраны все таски, которые нужно исполнить
 gulp.task('build', [
     'sprite:build',
     'fonts:build',
@@ -106,7 +116,7 @@ gulp.task('build', [
     'image:build'
 ]);
 
-
+// Таск для отслеживания изменений в файлах
 gulp.task('watch', function(){
     watch([path.watch.html], function(event, cb) {
         gulp.start('html:build');
@@ -125,4 +135,5 @@ gulp.task('watch', function(){
     });
 });
 
+// Дефолтный такс для запуска проекта
 gulp.task('default', ['build', 'webserver', 'watch']);
